3371b33be532a402e6111bbfd6a6f8006a9996dd
Kal <kalitinkers@gmail.com>
2024-12-10 22:19:53 +0200

    Update no.cpp

--- no.cpp
+++ no.cpp
@@ -9,7 +9,7 @@ std::complex<float> pluginEquation(const std::vector<float> coefficients, std::c
  int degree = coefficients.size() - 1;
  for (int i = 0; i <= degree; i++)
  {
-  result += coefficients[i] * std::pow(x, i);
+		result += coefficients[i] * std::pow(x, i);
  }
  return result;
 }
@@ -20,15 +20,45 @@ std::complex<float> deriveEquation(const std::vector<float> coefficients, std::c
  int degree = coefficients.size() - 1;
  for (int i = 1; i <= degree; i++)
  {
-  result += i * coefficients[i] * std::pow(x, i - 1);
+		result += i * coefficients[i] * std::pow(x, i - 1);
  }
  return result;
+}
+
+std::complex<float> newton(const std::vector<float> &coefficients, std::complex<float> initialGuess, float tolerance, int maxIterations)
+{
+
+ std::complex<float> x = initialGuess;
+
+ for (int i = 0; i < maxIterations; ++i)
+ {
+		std::complex<float> fx = pluginEquation(coefficients, x);
+		std::complex<float> fPrimeX = deriveEquation(coefficients, x);
+
+		/*if (std::abs(fPrimeX) < 1e-10f) // Small derivative check
+		{
+			std::cerr << "Derivative is too small. Stopping to prevent division by zero." << '\n';
+			break;
+		}
+*/
+		std::complex<float> xNew = x - fx / fPrimeX;
+
+		if (std::abs(xNew - x) < tolerance) // Convergence check
+		{
+			return xNew;
+		}
+
+		x = xNew;
+ }
+
+ return x; // Return last computed value
 }
 
 int main()
 {
  int degree{};
-
+ float tolerance = 1e-6f;
+ std::complex<float> initialGuess = 0.001f;
  std::cout << "Enter the degree of the polynomial";
  std::cin >> degree;
 
@@ -36,7 +66,7 @@ int main()
 
  for (int i = degree; i >= 0; --i)
  {
-  std::cout << "Enter coefficient of x^" << i << " ";
-  std::cin >> coefficients[i];
+		std::cout << "Enter coefficient of x^" << i << " ";
+		std::cin >> coefficients[i];
  }
 }
